25aab393d3aadc06960a5b1d074ed27d9a563752
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-08-29 14:06:55 +0530

    Update openload.co.index.php

--- hosts/upload/openload.co.index.php
+++ hosts/upload/openload.co.index.php
@@ -1,5 +1,5 @@
 <?php
 $upload_services[]="openload.co";
-$max_file_size["openload.co"]=1024; //I don't know it
+$max_file_size["openload.co"]=10240; //I don't know it
 $page_upload["openload.co"] = "openload.co.php";  
 ?>
