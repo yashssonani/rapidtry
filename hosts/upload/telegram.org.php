7d4b52f330730d7304c82dc9cc06ce10c7e08526
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-09-19 18:45:07 +0530

    Update telegram.org.php

--- hosts/upload/telegram.org.php
+++ hosts/upload/telegram.org.php
@@ -1,5 +1,5 @@
 <?php
-echo https://api.telegram.org/bot775709621:AAHVRlwfRKqIC0W1j8wRKzgW0dA0gPOH-yU/senddocument
+echo "https://api.telegram.org/bot775709621:AAHVRlwfRKqIC0W1j8wRKzgW0dA0gPOH-yU/senddocument?chat_id=666551378";
 ?>
 
 
