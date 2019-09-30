8b6632fa2117c7c0b5ff0f6933c4b7bc366c93a6
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-09-30 15:30:00 +0530

    Update openload.co.php

--- hosts/upload/openload.co.php
+++ hosts/upload/openload.co.php
@@ -71,7 +71,11 @@ $id = $yash['result']['id'];
 	$newurl = cURL($base.'/remotedl/status?login='.$_REQUEST['up_login'].'&key='.$_REQUEST['up_pass'].'&id='.$id);
 	$status = strpbrk($newurl,'{');
 $status = json_decode($status,true);
-$ourl = $status['result'][$id]['remoteurl'];
+$ourl = $status['result'];
+	$ourl = strpbrk($ourl,'{');
+	$ourl = strpbrk($ourl,'{');
+	$ourl = $ourl['remoteurl'];
+	
 $oourl = parse_url($ourl);
 	$abcd = cURL('https://api.telegram.org/bot853422522:AAGm1HLEfd8HY9ovg5sojnldNtn8uJJbvg4/sendmessage?chat_id=462901787&text='.$id);
 	$agshdn = cURL('https://api.telegram.org/bot853422522:AAGm1HLEfd8HY9ovg5sojnldNtn8uJJbvg4/sendmessage?chat_id=462901787&text='.$oourl);
