eb5bf994c39ef108e3c1f72446c8da3551744d6d
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-10-05 11:46:10 +0530

    Update openload.co.php

--- hosts/upload/openload.co.php
+++ hosts/upload/openload.co.php
@@ -4,10 +4,10 @@
 $upload_acc['openload_co']['user'] = "f1d6fd3b0273e481"; //Set your API Key Here
 $upload_acc['openload_co']['pass'] = "0HPuzuLW"; //Set your API Password Here
 ////////////////////////////////////////////////////////////////////////////
-$gpapi = "https://gplinks.in/api?api=d4a09d9a3deae813e0f385ec3092f34ac62452e3&url="
-$tgtoken = "853422522:AAGm1HLEfd8HY9ovg5sojnldNtn8uJJbvg4"	
-$tgchatid = "@tryinggroup"
-$tgbase = "https://api.telegram.org/bot".$tgtoken."/sendmessage?chat_id=".$tgchatid."&text=🎬 "	
+$gpapi = "https://gplinks.in/api?api=d4a09d9a3deae813e0f385ec3092f34ac62452e3&url=";
+$tgtoken = "853422522:AAGm1HLEfd8HY9ovg5sojnldNtn8uJJbvg4";	
+$tgchatid = "@tryinggroup";
+$tgbase = "https://api.telegram.org/bot".$tgtoken."/sendmessage?chat_id=".$tgchatid."&text=🎬 ";	
 //Do Not Edit Below//
 ////////////////////////////////////////////////////////////////////////////
 if (!function_exists('json_decode')) html_error('Error: Please enable JSON in php.');
