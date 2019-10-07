9259d196a9c68f42346e9e5a3377a3924d1739a1
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-10-07 14:44:56 +0530

    Update setup.php

--- configs/setup.php
+++ configs/setup.php
@@ -20,12 +20,16 @@ if (is_file(CONFIG_DIR.'config.php')) {
 	require_once(CONFIG_DIR.'config.php');
 	
 	
-		set($default_options);
+		if (count($options) == count($default_options)) {
+		unset($default_options);
+		return;
+	}
 		
 	
 }
 
 // Avoid setup page to be cached
+/*
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate ("D, d M Y H:i:s") . 'GMT');
 header('Cache-Control: max-age=0, no-store, no-cache, must-revalidate, proxy-revalidate, post-check=0, pre-check=0');
@@ -480,6 +484,7 @@ $d->close();
 	</tr>
 </table>
 </form>
+	*/
 <?php
 }
 
